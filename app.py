6573c3f31b34bc081be7052656c0607cea3d09f8
Juliana Medella Pereira <juliana.medella@cmb.gov.br>
2024-12-13 13:31:20 -0300

    chore: configura banco de dados com SQLAlchemy e Flask-Migrate

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def home():
+    return {"message": "API SecureTrack ativa"}
+
+if __name__ == '__main__':
+    app.run(debug=True)
