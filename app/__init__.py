aec4211af3d6b89b28094a0d8671980fcf707b28
Juliana Medella Pereira <juliana.medella@cmb.gov.br>
2024-12-14 19:33:54 -0300

    fix: adicao do CORS e imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from flask import Response
+
 
 # Inicializar extensões
 db = SQLAlchemy()
 ma = Marshmallow()
 
+
+
+
 def create_app():
     app = Flask(__name__)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guardium_mvp.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
 
     db.init_app(app)
     ma.init_app(app)
+
+    CORS(app)
 
     # Importar blueprints
     from app.controllers.ativos import ativos_bp
@@ -20,6 +30,7 @@ def create_app():
     from app.controllers.conformidades import conformidade_bp
 
     # Registrar blueprints
+    
     app.register_blueprint(ativos_bp, url_prefix='/ativos')
     app.register_blueprint(controles_bp, url_prefix='/controles')
     app.register_blueprint(conformidade_bp, url_prefix='/conformidade')
