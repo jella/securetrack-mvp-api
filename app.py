d33a6f8317fcc5a84cf243eca9bc04393a4ece48
Juliana Medella Pereira <juliana.medella@cmb.gov.br>
2024-12-14 13:07:59 -0300

    feat: adiciona arquivos de Inicialização da aplicação

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+from flask import Flask
 from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run('127.0.0.1','3001',debug=True)
--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from flask_marshmallow import Marshmallow
 
-# Inicializa o SQLAlchemy
+# Inicializar extensões
 db = SQLAlchemy()
+ma = Marshmallow()
 
 def create_app():
-    """Função de fábrica para criar e configurar a aplicação Flask."""
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guardium_mvp.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Inicializa o SQLAlchemy com o app
     db.init_app(app)
+    ma.init_app(app)
 
-    # Cria as tabelas no banco de dados
+    # Importar blueprints
+    from app.controllers.ativos import ativos_bp
+    from app.controllers.controles import controles_bp
+    from app.controllers.conformidades import conformidade_bp
+
+    # Registrar blueprints
+    app.register_blueprint(ativos_bp, url_prefix='/ativos')
+    app.register_blueprint(controles_bp, url_prefix='/controles')
+    app.register_blueprint(conformidade_bp, url_prefix='/conformidade')
+
+    # Criar as tabelas
     with app.app_context():
         db.create_all()
 
