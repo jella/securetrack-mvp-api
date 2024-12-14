5b9de8717e4c1101c7f1240b2a4fc774c6c506e2
Juliana Medella Pereira <juliana.medella@cmb.gov.br>
2024-12-14 13:05:04 -0300

    feat: adiciona modelos

--- app/models/conformidade.py
+++ app/models/conformidade.py
@@ -1,18 +1,17 @@
+from app import db
+
 class Conformidade(db.Model):
-    """
-    Modelo para representar a conformidade entre ativos e controles.
-    """
-    __tablename__ = 'conformidade'
+    __tablename__ = 'conformidades'
 
     id = db.Column(db.Integer, primary_key=True)
     ativo_id = db.Column(db.Integer, db.ForeignKey('ativos.id'), nullable=False)
     controle_id = db.Column(db.Integer, db.ForeignKey('controles.id'), nullable=False)
-    status = db.Column(db.String(50), nullable=False)  # Conforme, Não Conforme, etc.
-    data_alteracao = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    status = db.Column(db.String(50), nullable=False)
+    data_hora_alteracao = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
-    # Relacionamentos
     ativo = db.relationship('Ativo', backref=db.backref('conformidades', lazy=True))
     controle = db.relationship('Controle', backref=db.backref('conformidades', lazy=True))
+
 
     def __repr__(self):
         return f'<Conformidade Ativo:{self.ativo_id} Controle:{self.controle_id}>'
