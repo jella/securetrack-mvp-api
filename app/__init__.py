fa3483bfbb360b0eadfe23b5a8f1dae53be264fa
Juliana Medella Pereira <juliana.medella@cmb.gov.br>
2024-12-15 03:20:07 -0300

    fix: Altera o nome do banco

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ ma = Marshmallow()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guardium_mvp.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
 
