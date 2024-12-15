2ae360b6f48c584de9a067a501521d67c066102d
Juliana Medella Pereira <juliana.medella@cmb.gov.br>
2024-12-15 03:19:07 -0300

    chore: remove arquivos não utilizados

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    """Configurações básicas do projeto."""
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
